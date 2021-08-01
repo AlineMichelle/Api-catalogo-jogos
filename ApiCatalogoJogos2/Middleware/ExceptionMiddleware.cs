7e5b260f336052ceceb0f158e39f3c9dd4b18f93
Aline Michelle <alinesantana1711@gmail.com>
2021-08-01 00:04:44 -0300

    Aplicação de catalogo de jogos

---
+++ ApiCatalogoJogos2/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ApiCatalogoJogos2.Middeware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ExceptionMiddleware(RequestDelegate proximo)
+        {
+            this.next = proximo;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch
+            {
+                await HandleExceptionAsync(context);
+            }
+        }
+
+        public static async Task HandleExceptionAsync(HttpContext context)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsJsonAsync(new { Message = " Ocorreu um erro durante sua solicitação, por favor, tente novamente mais tarde " });
+        }
+    }
+}
